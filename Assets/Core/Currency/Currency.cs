c80a77c9f6be92926c32dae69708e175cb3764bb
ionutdejeu <ionut.dejeu@gmail.com>
2020-11-05 19:35:41 +0200

    Currency and Scriptable Object

---
+++ Assets/Core/Currency/Currency.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Core.Currency
+{
+    public class Currency
+    {
+		/// <summary>
+		/// Occurs when currency changed.
+		/// </summary>
+		public event Action currencyChanged;
+
+		public int currentCurrency { get; protected set; }
+
+		/// <summary>
+		/// Adds the currency.
+		/// </summary>
+		/// <param name="increment">the change in currency</param>
+		public void AddCurrency(int increment)
+		{
+			ChangeCurrency(increment);
+
+		}
+
+		/// <summary>
+		/// Changes the currency.
+		/// </summary>
+		/// <param name="increment">the change in currency</param>
+		protected void ChangeCurrency(int increment)
+		{
+			if (increment != 0)
+			{
+				currentCurrency += increment;
+				if (currencyChanged != null)
+				{
+					currencyChanged();
+				}
+			}
+		}
+	}
+}
