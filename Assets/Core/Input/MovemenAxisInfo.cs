69ec95bd7822be049e7f7b2f0cc30e13d22f45aa
Ionut <ionut.dejeu@gamail.com>
2020-11-12 13:36:14 +0200

    Models and Player Movement

---
+++ Assets/Core/Input/MovemenAxisInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Core.Input
+{
+    public struct MovemenAxisInfo
+	{
+		public MoveDirection MovemoveDir;
+		public Vector2 moveVector { get; set; }
+
+	}
+}
